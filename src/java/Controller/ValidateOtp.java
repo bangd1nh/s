714a2d7b5cabf2c6bec65f28d4cf50c48fd0e5e9
Trương Quang Quyền <QuangQuyen1305@gmail.com>
2024-03-05 19:34:49 +0700

    OTP

---
+++ src/java/Controller/ValidateOtp.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Admin
+ */
+public class ValidateOtp extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int value=Integer.parseInt(request.getParameter("otp"));
+		HttpSession session=request.getSession();
+		int otp=(int)session.getAttribute("otp");
+		String emails = (String)session.getAttribute("email");
+		RequestDispatcher dispatcher=null;
+		
+		
+		if (value==otp) 
+		{
+				request.setAttribute("email", emails);
+				request.setAttribute("status", "success");
+			  dispatcher=request.getRequestDispatcher("newpassword.jsp");
+			dispatcher.forward(request, response);
+			
+		}
+		else
+		{
+			request.setAttribute("message","wrong otp");
+			
+		   dispatcher=request.getRequestDispatcher("EnterOTP.jsp");
+			dispatcher.forward(request, response);
+		
+		}
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
